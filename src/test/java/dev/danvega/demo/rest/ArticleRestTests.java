fa490ed37d5f63478faafb600d997365e6b991f5
Mario Gray <mario.gray@gmail.com>
2022-09-27 19:02:02 -0700

    test

---
+++ src/test/java/dev/danvega/demo/rest/ArticleRestTests.java
@@ -0,0 +1,27 @@
+package dev.danvega.demo.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
+@AutoConfigureMockMvc
+public class ArticleRestTests {
+
+    @Test
+    public void test_should_return_article(@Autowired
+                                           MockMvc mockMvc) throws Exception {
+        mockMvc
+                .perform(get("/api/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("@[0].slug").value("hello-world"));
+    }
+
+}
